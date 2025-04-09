2cfcfd7dc78280b5f3ceffb98f5ccad697e08b7b
Berk Anbaroglu <banbar@hacettepe.edu.tr>
2025-04-09 13:59:16 +0300

    Point class integration

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,27 @@
 import csv
 
+class point:
+    def __init__(self, id, latitude, longitude, name):
+        self.id = id
+        self.lat = latitude
+        self.lon = longitude
+        self.name = name
+
 def open_points(file):
     data_list = []
+
     with open(file, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        header = next(csv_reader)  
+        header = next(csv_reader)  # Skip/save the header row if needed
         
         for row in csv_reader:
-            data_list.append(row)  
-
-    return data_list
+            data_list.append(row)  # Add each row to the list
+    
+    poi_list = []
+    for i in range(len(data_list)):
+        p = point(data_list[i][0], 
+                  float(data_list[i][1]),
+                  float(data_list[i][2]),
+                  data_list[i][3])
+        poi_list.append(p)
+    return poi_list
